41afc723004620c0e00ae920215888abf7696841
Admin <admin@Admins-MacBook-Pro.local>
2021-10-02 18:57:45 +0100

    Reconfigured SpecRunner file

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -3,4 +3,12 @@ describe("BowlingGame", () => {
         let bowlinggame => BowlingGame.make();
         bowlinggame.roll(0);
     });
+
+    it("can score a gutter game", () => {
+        let bowlinggame = BowlingGame.make();
+        for (var i = 0; i < 20; i++) {
+            bowlinggame.roll(0);
+        }
+        expect(game.score()).toBe(0);
+    });
 });
